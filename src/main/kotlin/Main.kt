01c558d215eefd73682a6fd8200e64355d85fc37
kor-Chipmunk <rhj4862@gmail.com>
2021-04-18 20:45:43 +0900

    feat: iOS 공고 알림을 추가합니다.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -113,11 +113,11 @@ data class Author(
 
 data class Image(val url: String)
 
-const val CATEGORY = 872
-const val COLLECTION = "spring-jobs"
+const val CATEGORY = 678
+const val COLLECTION = "ios-jobs"
 
 const val WANTED_JOB_SEARCH_API_URL = "https://www.wanted.co.kr/api/v4/jobs?1617705029342&country=kr&tag_type_id=$CATEGORY&job_sort=job.latest_order&locations=all&years=-1"
 const val LAST_VIEWED_ID_API_URL = "https://wantedsauron-cb29.restdb.io/rest/$COLLECTION?sort=_id&dir=-1"
 
-val ENV_KEY_DISCORD_WEBHOOKS = arrayOf("DISCORD_WEBHOOK", "DISCORD_SPRING_WEBHOOK")
+val ENV_KEY_DISCORD_WEBHOOKS = arrayOf("DISCORD_WEBHOOK", "DISCORD_IOS_WEBHOOK")
 const val ENV_KEY_REST_DB_KEY = "REST_DB_KEY"
